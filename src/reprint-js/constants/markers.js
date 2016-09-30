0f5983c18c5ae78c84af85835cdb6f34904af9d3
Andres Suarez <zertosh@gmail.com>
2016-09-30 16:55:38 -0400

    Setup watched builds

--- src/reprint-js/constants/markers.js
+++ src/reprint-js/constants/markers.js
@@ -1,12 +1,11 @@
-'use babel';
-/* @flow */
-
 /*
  * Copyright (c) 2015-present, Facebook, Inc.
  * All rights reserved.
  *
  * This source code is licensed under the license found in the LICENSE file in
  * the root directory of this source tree.
+ *
+ * @flow
  */
 
 // These are special markers that can be used to identify special handling.
