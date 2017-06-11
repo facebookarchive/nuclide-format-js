1980dee3752e6b84994f48dd1be68874830d8c97
xixixao <xixixao@seznam.cz>
2017-06-11 22:39:23 +0100

    Remove reprint option in favor of prettier

--- src/atom/formatCode.js
+++ src/atom/formatCode.js
@@ -15,8 +15,7 @@ import type {SourceOptions} from '../common/options/SourceOptions';
 import updateCursor from '../update-cursor';
 
 async function formatCode(options: SourceOptions, editor_: ?TextEditor): Promise<void> {
-  let editor = editor_;
-  editor = editor || atom.workspace.getActiveTextEditor();
+  const editor = editor_ || atom.workspace.getActiveTextEditor();
   if (!editor) {
     // eslint-disable-next-line no-console
     console.log('- format-js: No active text editor');
@@ -25,36 +24,23 @@ async function formatCode(options: SourceOptions, editor_: ?TextEditor): Promise
 
   // Save things
   const buffer = editor.getBuffer();
-  const oldSource = buffer.getText();
-  let source = oldSource;
-
-  // Reprint transform.
-  if (atom.config.get('nuclide-format-js.reprint')) {
-    const reprint = require('../reprint-js');
-    // $FlowFixMe(kad) -- this seems to conflate an class instance with an ordinary object.
-    const reprintResult = reprint(source, {
-      maxLineLength: 80,
-      useSpaces: true,
-      tabWidth: 2,
-    });
-    source = reprintResult.source;
-  }
+  const inputSource = buffer.getText();
 
   // Auto-require transform.
   // TODO: Add a limit so the transform is not run on files over a certain size.
   const {transform} = require('../common');
-  source = transform(source, options);
+  const outputSource = transform(inputSource, options);
 
   // Update the source and position after all transforms are done. Do nothing
   // if the source did not change at all.
-  if (source === oldSource) {
+  if (outputSource === inputSource) {
     return;
   }
 
   const range = buffer.getRange();
   const position = editor.getCursorBufferPosition();
-  editor.setTextInBufferRange(range, source);
-  const {row, column} = updateCursor(oldSource, position, source);
+  editor.setTextInBufferRange(range, outputSource);
+  const {row, column} = updateCursor(inputSource, position, outputSource);
   editor.setCursorBufferPosition([row, column]);
 
   // Save the file if that option is specified.
