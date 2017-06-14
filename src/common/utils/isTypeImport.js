fb7cdbd2edf39d9a066eb216435f0588e2ec621e
xixixao <xixixao@seznam.cz>
2017-06-14 01:39:54 +0100

    Missing files from PR #15

---
+++ src/common/utils/isTypeImport.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ *
+ * @flow
+ */
+
+import type {NodePath} from '../types/ast';
+
+/**
+ * f.e.: import type Foo from 'Foo';
+ */
+function isTypeImport(path: NodePath): boolean {
+  return path.value.importKind === 'type';
+}
+
+module.exports = isTypeImport;
