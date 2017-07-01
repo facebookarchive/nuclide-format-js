5504a0a1c5453da1ed78b423c3fe3c6d571998bb
Michal Srb <xixixao@seznam.cz>
2017-07-01 21:13:11 +0100

    Remove unused types from destructured imports (#31)

--- __tests__/requiresTransform-spec.js
+++ __tests__/requiresTransform-spec.js
@@ -65,6 +65,7 @@ const TESTS = [
   'remove-shadowed-requires',
   'remove-shadowed-types',
   'remove-unused-array-patterns',
+  'remove-unused-destructured-types',
   'remove-unused-requires',
   'remove-unused-types',
   'respect-declaration-kind',
