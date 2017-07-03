714fee539f3a2a6551790daceb3e2cd6d1202f13
Michal Srb <xixixao@seznam.cz>
2017-07-03 21:01:28 +0100

    Do not remove existing JSX requires (#39)

--- src/common/utils/getJSXIdentifiers.js
+++ src/common/utils/getJSXIdentifiers.js
@@ -10,12 +10,8 @@
 
 import type {Collection} from '../types/ast';
 
+import getJSXIdentifierName from './getJSXIdentifierName';
 import jscs from 'jscodeshift';
-import {isLowerCase} from './StringUtils';
-
-// TODO: make this configurable somehow, we probably don't want to explicitly
-// list out all of the lowercase html tags that are built-in
-const LOWER_CASE_WHITE_LIST = new Set(['fbt']);
 
 /**
  * This will get a list of identifiers for JSXElements in the AST
@@ -26,13 +22,10 @@ function getJSXIdentifiers(root: Collection): Set<string> {
     // There should be an opening element for every single closing element so
     // we can just look for opening ones
     .find(jscs.JSXOpeningElement)
-    .filter(path => jscs.JSXIdentifier.check(path.node.name))
     .forEach(path => {
-      const name = path.node.name.name;
-      // TODO: should this be here or in addMissingRequires?
-      if (!isLowerCase(name) || LOWER_CASE_WHITE_LIST.has(name)) {
-        ids.add(name);
-      }
+      getJSXIdentifierName(path).forEach(node => {
+        ids.add(node.name);
+      });
     });
   return ids;
 }
